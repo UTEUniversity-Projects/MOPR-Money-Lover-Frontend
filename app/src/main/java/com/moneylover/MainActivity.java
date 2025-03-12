37b349c38794e88a8c70e53428db829d3e588a7d
LeTanTru <letantru@gmail.com>
2025-03-12 14:24:44 +0700

    add: intro and splash screen

--- app/src/main/java/com/moneylover/MainActivity.java
+++ app/src/main/java/com/moneylover/MainActivity.java
@@ -1,6 +1,13 @@
 package com.moneylover;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,63 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.moneylover.ui.StartActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN = 2000;
+
+    private Animation zoomOutAnimation;
+    private ImageView image;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private final Runnable splashRunnable = () -> {
+        Intent intent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(intent);
+        finish();
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        image = findViewById(R.id.imgVLogo);
+
+        Animation zoomInAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_in_animation);
+        zoomOutAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_out_animation);
+
+        zoomInAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                image.startAnimation(zoomOutAnimation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        image.startAnimation(zoomInAnimation);
+
+        handler.postDelayed(splashRunnable, SPLASH_SCREEN);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(splashRunnable);
     }
 }
